1aba9557f840d90ab430109ca2146ee1795e19db
Alper Dursun <61089742+nypex5710@users.noreply.github.com>
2020-04-24 23:46:08 +0300

    fix typo

--- src/pocketmine/item/Elytra.php
+++ src/pocketmine/item/Elytra.php
@@ -4,9 +4,13 @@ namespace pocketmine\item;
 
 const SLOT_NUMBER = 1;
 
-class Elytra extends Armor{
+class Elytra extends Item{
 
-    public function __construct($meta = 0, $count = 1){
-        parent::__construct(self::ELYTRA, $meta, $count, "Elytra");
+    public function __construct(int $meta = 0){
+        parent::__construct(Item::ELYTRA, $meta, "Elytra");
+    }
+
+    public function getMaxStackSize(): int{
+        return 1;
     }
 }
